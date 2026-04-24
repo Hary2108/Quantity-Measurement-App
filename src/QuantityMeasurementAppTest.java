9cd21ad2840e42c7b584579f493a961660579945
HARY2108 <hareeshsboa@gmail.com>
2026-04-24 23:51:18 +0530

    UC5:Unit-to-Unit Conversion (Same Measurement Type)

--- src/QuantityMeasurementAppTest.java
+++ src/QuantityMeasurementAppTest.java
@@ -4,48 +4,32 @@ import static org.junit.jupiter.api.Assertions.*;
 public class QuantityMeasurementAppTest {
 
     @Test
-    public void testEquality_YardToFeet_EquivalentValue() {
-        assertTrue(new Quantity(1.0, LengthUnit.YARD)
-                .equals(new Quantity(3.0, LengthUnit.FEET)));
+    public void testConversion_FeetToInch() {
+        assertEquals(12.0, Quantity.convert(1.0, LengthUnit.FEET, LengthUnit.INCH), 0.0001);
     }
 
     @Test
-    public void testEquality_YardToInches_EquivalentValue() {
-        assertTrue(new Quantity(1.0, LengthUnit.YARD)
-                .equals(new Quantity(36.0, LengthUnit.INCH)));
+    public void testConversion_InchToFeet() {
+        assertEquals(2.0, Quantity.convert(24.0, LengthUnit.INCH, LengthUnit.FEET), 0.0001);
     }
 
     @Test
-    public void testEquality_CentimeterToInch_EquivalentValue() {
-        assertTrue(new Quantity(1.0, LengthUnit.CENTIMETER)
-                .equals(new Quantity(0.393701, LengthUnit.INCH)));
+    public void testConversion_YardToInch() {
+        assertEquals(36.0, Quantity.convert(1.0, LengthUnit.YARD, LengthUnit.INCH), 0.0001);
     }
 
     @Test
-    public void testEquality_CentimeterToFeet_NonEquivalent() {
-        assertFalse(new Quantity(1.0, LengthUnit.CENTIMETER)
-                .equals(new Quantity(1.0, LengthUnit.FEET)));
+    public void testConversion_CentimeterToInch() {
+        assertEquals(1.0, Quantity.convert(2.54, LengthUnit.CENTIMETER, LengthUnit.INCH), 0.0001);
     }
 
     @Test
-    public void testEquality_MultiUnit_Transitive() {
-
-        Quantity yard = new Quantity(1.0, LengthUnit.YARD);
-        Quantity feet = new Quantity(3.0, LengthUnit.FEET);
-        Quantity inches = new Quantity(36.0, LengthUnit.INCH);
-
-        assertTrue(yard.equals(feet));
-        assertTrue(feet.equals(inches));
-        assertTrue(yard.equals(inches));
+    public void testConversion_Zero() {
+        assertEquals(0.0, Quantity.convert(0.0, LengthUnit.FEET, LengthUnit.INCH), 0.0001);
     }
 
     @Test
-    public void testEquality_AllUnits_ComplexScenario() {
-
-        assertTrue(new Quantity(2.0, LengthUnit.YARD)
-                .equals(new Quantity(6.0, LengthUnit.FEET)));
-
-        assertTrue(new Quantity(6.0, LengthUnit.FEET)
-                .equals(new Quantity(72.0, LengthUnit.INCH)));
+    public void testConversion_Negative() {
+        assertEquals(-12.0, Quantity.convert(-1.0, LengthUnit.FEET, LengthUnit.INCH), 0.0001);
     }
 }
